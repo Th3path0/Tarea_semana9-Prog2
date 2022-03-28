606490d9512fd75e0b9bb599ac4f31ccd4f91db0
Th3path0 <63733274+Th3path0@users.noreply.github.com>
2022-03-27 21:38:48 -0400

    Add files via upload

---
+++ Tema9BD.sql
@@ -0,0 +1,57 @@
+-- creaccion de BD
+Create Database Mant_product;
+
+-- Creación de tabla
+Create Table Categoria(
+IDcategoria int identity(1,1)primary key not null,
+Codigo AS ('CT'+RIGHT('00' + CONVERT(VARCHAR, IDcategoria), (2))),
+Nombre Nvarchar(30) not null,
+Descripcion Nvarchar(256) null
+
+);
+
+-- Insert
+Insert Into Categoria VALUES('Herramientas electricas','Cuenta con las mejores herremientas electrica, para cualquier trabajo');
+Insert Into Categoria VALUES('Herramientas manuales','Cuenta con las mejores herremientas, desde las más básica');
+
+-- select
+Select * from Categoria;
+
+--Store Procedure
+--1- Buscar
+Create PROC SP_BuscarCaterogria
+@Buscar Nvarchar(25)
+AS
+Select * From Categoria
+Where Nombre Like @Buscar + '%'
+
+go
+
+--Insertar
+Create PROC SP_InsertarCategoria
+@Nombre Nvarchar(35),
+@Descripcion Nvarchar(256)
+AS
+Insert Into Categoria Values (@Nombre, @Descripcion)
+ 
+ go
+
+--Editar
+Create PROC SP_editarCategoria
+@IDcategoria INT,
+@Nombre Nvarchar(35),
+@Descripcion Nvarchar(256)
+AS
+Update Categoria Set Nombre = @Nombre, Descripcion = @Descripcion
+WHERE IDcategoria = @IDcategoria
+
+go
+
+--SP Eliminar
+Create PROC SP_eliminarCategoria
+@IDcategoria INT
+AS
+Delete Categoria
+WHERE IDcategoria = @IDcategoria
+
+
